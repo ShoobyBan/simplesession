02a91f220f9f71cb0ae1f5e87d3736cfb6b8d173
Sam Ban <sam@gpmd.co.uk>
2018-01-26 20:25:09 +0000

    fix syntax and bugs

--- simplesession.go
+++ simplesession.go
@@ -6,14 +6,13 @@ import (
 )
 
 // SimpleSession type, for session save/load
-type SimpleSession struct {
-	storageFilePath string
-	m               map[string]interface{}
-}
+var StorageFilePath string
+
+type SimpleSession map[string]interface{}
 
 // Save encodes and saves session via Gob to file
-func (s SimpleSession) Save() error {
-	file, err := os.Create(s.storageFilePath)
+func (s *SimpleSession) Save() error {
+	file, err := os.Create(StorageFilePath)
 	if err == nil {
 		encoder := gob.NewEncoder(file)
 		encoder.Encode(s)
@@ -23,19 +22,24 @@ func (s SimpleSession) Save() error {
 }
 
 // Load loads decodes session from Gob file, saving will update the file
-func (s SimpleSession) Load(storageFilePath string) (SimpleSession, error) {
+func Load(storageFilePath string) (*SimpleSession, error) {
 	file, err := os.Open(storageFilePath)
+	s := new(SimpleSession)
 	if err == nil {
 		decoder := gob.NewDecoder(file)
 		err = decoder.Decode(s)
+	} else {
+		n := make(SimpleSession, 1)
+		s = &n
+		err = nil
 	}
-	s.storageFilePath = storageFilePath
+	StorageFilePath = storageFilePath
 	file.Close()
 	return s, err
 }
 
 // Set sets a value into the store and saves session
-func (s SimpleSession) Set(key string, val interface{}) {
-	s.m[key] = val
+func (s *SimpleSession) Set(key string, val interface{}) {
+	(*s)[key] = val
 	s.Save()
 }
